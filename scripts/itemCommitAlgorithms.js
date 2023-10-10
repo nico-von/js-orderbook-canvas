a123c877968d7ac5339d5504d29d5be2a55c04bd
nico-von <131730901+nico-von@users.noreply.github.com>
2023-10-10 17:56:14 +0800

    Move commit algorithms to their own files
    This leaves the binance order book management algorithms free from
    custom code and focuses on the recommended binance api order book
    management process alone.

---
+++ scripts/itemCommitAlgorithms.js
@@ -0,0 +1,55 @@
+import { roundToNearestTick } from "./numberManipulationFunctions.js";
+
+export function commitToDepth(snapshot, lobDepth) {
+    // populate accordingly
+    let bids = snapshot.e ? snapshot.b : snapshot.bids;
+    let asks = snapshot.e ? snapshot.a : snapshot.asks;
+  
+    function iter(arr, type) {
+      for (let item of arr) {
+        // we need to save these according to
+        // client tick sizes
+        if (!lobDepth.clientTickSize) {
+          commitItemWithoutClientTick(item, type, lobDepth);
+        } else {
+          commitItemWithClientTick(item, type, lobDepth);
+        }
+      }
+    }
+    iter(bids, "bid");
+    iter(asks, "ask");
+  }
+
+
+function commitItemWithoutClientTick(item, type, lobDepth) {
+    let price = item[0];
+    let qty = parseFloat(item[1]);
+  
+    if (type == "bid") {
+      if (qty === 0) {
+        delete lobDepth.bids[price];
+        return;
+      }
+  
+      lobDepth.bids[price] = {
+        price,
+        qty,
+        priceFloat: parseFloat(price),
+      };
+    } else if (type == "ask") {
+      if (qty === 0) {
+        delete lobDepth.asks[price];
+        return;
+      }
+  
+      lobDepth.asks[price] = {
+        price,
+        qty,
+        priceFloat: parseFloat(price),
+      };
+    }
+  }
+  
+  function commitItemWithClientTick(item, type, lobDepth) {
+  
+  }
